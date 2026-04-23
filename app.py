bbcb99b4ae99809a202f058796ae12d135260798
eloahsilva <eloah2412@gmail.com>
2026-04-23 13:04:37 +0000

    Alterações aula 23/04

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,7 +9,7 @@ def index():
 @app.route('/faleconosco')
 @app.route('/contato')
 def contato():
-    dados={'nome: Eloah', 'email: silva.eloah@escolar.ifrn.edu.br'}
+    dados={'nome': 'Eloah', 'email': 'silva.eloah@escolar.ifrn.edu.br'}
     return render_template('contato.html', dados=dados)
 
 @app.route('/usuario', defaults={'nome': 'Desconhecido', 'sobrenome': 'Desconhecido'})
@@ -24,6 +24,60 @@ def semestre(x):
     dados['atual'] = x
     dados['anterior'] = x-1
     return render_template('semestre.html', dados=dados)
+
+@app.route('/perfil/<usuario>', defaults={'usuario': 'eloah'})
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    return render_template('perfil.html', usuario=usuario)
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+@app.route('/recebedados', methods=['GET', 'POST'])
+def recebedados():
+    nome = request.form.get['nome']
+    sobrenome = request.form['sobrenome']
+    email = request.form['email']
+    estado = request.form.get('estado')
+    escola = request.form.getlist('escola')
+    return render_template('recebedados.html', nome=nome, sobrenome=sobrenome, email=email, estado=estado, escola=escola)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/verificarlogin', methods=['POST'])
+def verificarlogin():
+    usuario = request.form.get('login')
+    senha = request.form.get('senha')
+    if usuario == 'admin' and senha == '12345':
+        return redirect(url_for('arearestrita'))
+    else:
+        return redirect(url_for('acessonegado'))
+
+@app.route('/arearestrita')
+def arearestrita():
+    return render_template('arearestrita.html')
+
+@app.route('/exemplolaco')
+def exemplolaco():
+    return render_template('exemplolaco.html')
+
+@app.route('/acessonegado')
+def acessonegado():
+    return render_template('acessonegado.html')
+
+@app.route('/verificaridade2/<int:idade>')
+def verificaridade2(idade):
+    return render_template('idade.html', idade=idade)
+
+@app.route('/produtos')
+def produtos():
+    itens=[{'nome':'Teclado', 'preco':'200'}, 
+        {'nome':'Smartphone', 'preco':'4500'},
+        {'nome':'Pen-drive', 'preco':'50'}]
+    qtd = len(itens)
+    return render_template('produtos.html', itens=itens, qtd=qtd)
 
 if __name__ == '__main__':
     app.run()
